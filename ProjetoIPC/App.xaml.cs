96ce68cd7fa5315c92ac21e2e7d8f676b599582f
MARCO IVAN PAIS SANTOS <al77291@alunos.utad.pt>
2025-05-07 15:32:43 +0100

    Add project files.

---
+++ ProjetoIPC/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjetoIPC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ ProjetoIPC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoIPC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
