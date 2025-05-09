904a837a69beef81ec268f4d2d61609564920a82
RUI MEIRELES <al77199@alunos.utad.pt>
2025-05-09 01:06:36 +0100

    view latestdrives

---
+++ ProjetoIPC/Views/LatestDrives.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace ProjetoIPC.Views;
+
+public partial class LatestDrives : ContentPage
+{
+    public LatestDrives()
+    {
+        InitializeComponent();
+    }
+}
